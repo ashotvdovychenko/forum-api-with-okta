2bca10c4a1373f71d280e1021ce42eca69d9eb7d
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-07-31 07:06:50 +0300

    Add JPA repositories

---
+++ src/main/java/com/example/forum/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.forum.api.repository;
+
+import com.example.forum.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
