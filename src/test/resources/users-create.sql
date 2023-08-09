682c1962dfead3b78a1c2027aad4dd9be9b86864
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-08-09 15:26:08 +0300

    Add tests of entities lifecycle

---
+++ src/test/resources/users-create.sql
@@ -0,0 +1,6 @@
+insert into USERS (id, username, password, created_at)
+values (1, 'first', 'password', now()),
+       (2, 'second', 'password', now()),
+       (3, 'third', 'password', now()),
+       (4, 'fourth', 'password', now()),
+       (5, 'fifth', 'password', now());
