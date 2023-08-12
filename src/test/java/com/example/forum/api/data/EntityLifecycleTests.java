1f38639bac2ca711b72f3e8ec82f0cf6763b97c8
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-08-12 04:36:08 +0300

    Code refactor and checkstyle fix (#11)
    * Code refactor
    
    * Fix checkstyle job

--- src/test/java/com/example/forum/api/data/EntityLifecycleTests.java
+++ src/test/java/com/example/forum/api/data/EntityLifecycleTests.java
@@ -1,139 +1,138 @@
 package com.example.forum.api.data;
 
+import static com.example.forum.api.data.JpaRepositoryUtils.findById;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.example.forum.api.domain.Comment;
 import com.example.forum.api.domain.Topic;
 import com.example.forum.api.repository.CommentRepository;
 import com.example.forum.api.repository.TopicRepository;
 import com.example.forum.api.repository.UserRepository;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.List;
-
-import static com.example.forum.api.data.JpaRepositoryUtils.findById;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EntityLifecycleTests {
-    @Autowired
-    private CommentRepository commentRepository;
+  @Autowired
+  private CommentRepository commentRepository;
 
-    @Autowired
-    private TopicRepository topicRepository;
+  @Autowired
+  private TopicRepository topicRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql"})
-    public void addCommentToTopicAndUserCreatesComment() {
-        var topic = findById(1L, topicRepository);
-        var user = findById(1L, userRepository);
-        var newComment = new Comment();
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql"})
+  public void addCommentToTopicAndUserCreatesComment() {
+    var topic = findById(1L, topicRepository);
+    var user = findById(1L, userRepository);
+    var newComment = new Comment();
 
-        user.addComment(newComment);
-        topic.addComment(newComment);
+    user.addComment(newComment);
+    topic.addComment(newComment);
 
-        assertFalse(commentRepository.findAll().isEmpty());
-    }
+    assertFalse(commentRepository.findAll().isEmpty());
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void deleteCommentDoesNotDeleteTopicAndUser() {
-        commentRepository.deleteById(9L);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void deleteCommentDoesNotDeleteTopicAndUser() {
+    commentRepository.deleteById(9L);
 
-        assertFalse(commentRepository.existsById(9L));
-        assertTrue(userRepository.existsById(1L));
-        assertTrue(topicRepository.existsById(1L));
-    }
+    assertFalse(commentRepository.existsById(9L));
+    assertTrue(userRepository.existsById(1L));
+    assertTrue(topicRepository.existsById(1L));
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void deleteCommentFromUserDeletesComment() {
-        var user = findById(1L, userRepository);
-        var comment = findById(9L, commentRepository);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void deleteCommentFromUserDeletesComment() {
+    var user = findById(1L, userRepository);
+    var comment = findById(9L, commentRepository);
 
-        user.removeComment(comment);
+    user.removeComment(comment);
 
-        assertFalse(commentRepository.existsById(9L));
-    }
+    assertFalse(commentRepository.existsById(9L));
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void deleteCommentFromTopicDeletesComment() {
-        var topic = findById(1L, topicRepository);
-        var comment = findById(9L, commentRepository);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void deleteCommentFromTopicDeletesComment() {
+    var topic = findById(1L, topicRepository);
+    var comment = findById(9L, commentRepository);
 
-        topic.removeComment(comment);
+    topic.removeComment(comment);
 
-        assertFalse(commentRepository.existsById(9L));
-    }
+    assertFalse(commentRepository.existsById(9L));
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void deleteUserDeletesComments() {
-        userRepository.deleteById(1L);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void deleteUserDeletesComments() {
+    userRepository.deleteById(1L);
 
-        assertFalse(commentRepository.existsById(9L));
-        assertFalse(commentRepository.existsById(4L));
-    }
+    assertFalse(commentRepository.existsById(9L));
+    assertFalse(commentRepository.existsById(4L));
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void deleteTopicDeletesComments() {
-        topicRepository.deleteById(1L);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void deleteTopicDeletesComments() {
+    topicRepository.deleteById(1L);
 
-        assertFalse(commentRepository.existsById(9L));
-        assertFalse(commentRepository.existsById(6L));
-        assertFalse(commentRepository.existsById(3L));
-    }
+    assertFalse(commentRepository.existsById(9L));
+    assertFalse(commentRepository.existsById(6L));
+    assertFalse(commentRepository.existsById(3L));
+  }
 
-    @Test
-    @Sql("/users-create.sql")
-    public void addTopicToUserCreatesTopic() {
-        var user = findById(1L, userRepository);
-        var newTopic = new Topic();
+  @Test
+  @Sql("/users-create.sql")
+  public void addTopicToUserCreatesTopic() {
+    var user = findById(1L, userRepository);
+    var newTopic = new Topic();
 
-        user.addCreatedTopic(newTopic);
+    user.addCreatedTopic(newTopic);
 
-        assertFalse(topicRepository.findAll().isEmpty());
-    }
+    assertFalse(topicRepository.findAll().isEmpty());
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql"})
-    public void deleteTopicFromUserDeletesTopic() {
-        var user = findById(1L, userRepository);
-        var topic = findById(1L, topicRepository);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql"})
+  public void deleteTopicFromUserDeletesTopic() {
+    var user = findById(1L, userRepository);
+    var topic = findById(1L, topicRepository);
 
-        user.removeCreatedTopic(topic);
+    user.removeCreatedTopic(topic);
 
-        assertFalse(topicRepository.existsById(1L));
-    }
+    assertFalse(topicRepository.existsById(1L));
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void deleteUserDeletesTopic() {
-        userRepository.deleteById(1L);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void deleteUserDeletesTopic() {
+    userRepository.deleteById(1L);
 
-        assertFalse(topicRepository.existsById(1L));
-    }
+    assertFalse(topicRepository.existsById(1L));
+  }
 
-    @Test
-    @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
-    public void commentsInTopicAreSortedByCreationTime() {
-        var actualTopicCommentsIds = findById(1L, topicRepository)
-                .getComments()
-                .stream()
-                .map(Comment::getId)
-                .toList();
-        var expectedTopicCommentsIds = List.of(3L, 6L, 9L);
+  @Test
+  @Sql({"/users-create.sql", "/topics-create.sql", "/comments-create.sql"})
+  public void commentsInTopicAreSortedByCreationTime() {
+    var actualTopicCommentsIds = findById(1L, topicRepository)
+        .getComments()
+        .stream()
+        .map(Comment::getId)
+        .toList();
+    var expectedTopicCommentsIds = List.of(3L, 6L, 9L);
 
-        assertThat(actualTopicCommentsIds).isEqualTo(expectedTopicCommentsIds);
-    }
+    assertThat(actualTopicCommentsIds).isEqualTo(expectedTopicCommentsIds);
+  }
 }
