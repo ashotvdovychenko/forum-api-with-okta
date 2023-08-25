4ff09cd6794ef90d5445fbfab2bb43a1c72f5572
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-08-25 18:11:54 +0300

    Maven checkstyle profile (#14)
    * Configured checkstyle profile
    
    * Refactored code according to checkstyle

--- src/main/java/com/example/forum/api/ForumApiWithOktaApplication.java
+++ src/main/java/com/example/forum/api/ForumApiWithOktaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ForumApiWithOktaApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(ForumApiWithOktaApplication.class, args);
   }
-
 }
