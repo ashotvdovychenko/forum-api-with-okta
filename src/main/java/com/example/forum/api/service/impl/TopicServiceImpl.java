16109b1f1c68075e47383e8e14861aefd7c24895
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-08-30 20:28:27 +0300

    Add comment manipulating methods

--- src/main/java/com/example/forum/api/service/impl/TopicServiceImpl.java
+++ src/main/java/com/example/forum/api/service/impl/TopicServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.forum.api.service.impl;
 
+import com.example.forum.api.domain.Comment;
 import com.example.forum.api.domain.Topic;
+import com.example.forum.api.repository.CommentRepository;
 import com.example.forum.api.repository.TopicRepository;
 import com.example.forum.api.repository.UserRepository;
 import com.example.forum.api.service.TopicService;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -17,6 +20,7 @@ public class TopicServiceImpl implements TopicService {
 
   private final TopicRepository topicRepository;
   private final UserRepository userRepository;
+  private final CommentRepository commentRepository;
 
   @Override
   @Transactional
@@ -44,5 +48,35 @@ public class TopicServiceImpl implements TopicService {
   @Override
   public void deleteById(Long id) {
     topicRepository.deleteById(id);
+  }
+
+  @Override
+  @Transactional
+  public Comment addComment(Comment comment, Long topicId, String creatorUsername) {
+    var topic = findById(topicId).orElseThrow(NoSuchElementException::new);
+    var user = userRepository.findByUsername(creatorUsername)
+        .orElseThrow(NoSuchElementException::new);
+    topic.addComment(comment);
+    user.addComment(comment);
+    return commentRepository.save(comment);
+  }
+
+  @Override
+  @Transactional
+  public List<Comment> getComments(Long topicId) {
+    return findById(topicId)
+        .map(Topic::getComments)
+        .map(List::copyOf)
+        .orElse(Collections.emptyList());
+  }
+
+  @Override
+  public Comment updateComment(Comment comment) {
+    return commentRepository.save(comment);
+  }
+
+  @Override
+  public void deleteCommentById(Long id) {
+    commentRepository.deleteById(id);
   }
 }
